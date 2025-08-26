506b16ce9b4d6494d0420b9d86ccae74361bdf3a
Junior Sequera <35280577+junseiken@users.noreply.github.com>
2025-08-26 04:44:17 +0000

    agg

--- src/app.js
+++ src/app.js
@@ -1,11 +1,35 @@
-import "bootstrap";
-import "./style.css";
+let pronoun = ['the', 'our'];
+let adj = ['great', 'big'];
+let noun = ['jogger', 'racoon'];
+let suffix = ['.com', '.es', '.org', '.mx', '.ve', '.net'];
+let domainNames = [];
 
+function generateDomains() {
+  domainNames = [];
+  for (let i = 0; i < pronoun.length; i++) {
+    for (let m = 0; m < adj.length; m++) {
+      for (let t = 0; t < noun.length; t++) {
+        for (let s = 0; s < suffix.length; s++) {
+          let domain = pronoun[i] + adj[m] + noun[t] + suffix[s];
+          domainNames.push(domain);
+        }
+      }
+    }
+  }
+}
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+function renderDomains() {
+  let html = '';
+  domainNames.forEach((domain, idx) => {
+    html += `<p>${idx + 1}. ${domain}</p>`;
+  });
+  html += `<p>Total: ${domainNames.length} dominios generados</p>`;
+  document.getElementById('Domains').innerHTML = html;
+  console.log(`Total de combinaciones generadas: ${domainNames.length}`);
+}
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+function generateDomain() {
+  generateDomains();
+  renderDomains();
+}
+window.generateDomain = generateDomain;
